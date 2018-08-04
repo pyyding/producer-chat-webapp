ba19cd469cf73480753b20c25f2c82ecc0506763
pyyding <kaspar.pyyding@gmail.com>
2018-08-04 17:28:40 +0300

    QA - Answers

--- components/qa/AnswerCard.vue
+++ components/qa/AnswerCard.vue
@@ -2,10 +2,13 @@
 	<div v-if="answer && user">
 		<v-layout row align-center>
 			<v-avatar class="mr-2" size="36px"><img :src="answer.user.photoURL" alt="avatar"></v-avatar>
-			<strong>{{answer.user.displayName}}</strong>
+			<div>
+				<strong>{{answer.user.displayName}}</strong>
+				<div><span class="grey--text">{{answer.createdAt.toDate().toLocaleDateString()}}</span></div>
+			</div>
 			<v-btn title="delete answer" icon v-if="user && user.id === answer.user.id" @click="deleteAnswer"><v-icon color="grey" small>clear</v-icon></v-btn>
 		</v-layout>
-		<v-layout row>
+		<v-layout row mt-2>
 			<p class="answer--text">
 				{{answer.text}}
 			</p>
@@ -22,7 +25,15 @@
 		methods: {
 			deleteAnswer () {
 				this.$store.dispatch('qa/deleteAnswer', this.answer.id)
+					.then(() => { this.$store.dispatch('qa/getQuestionData', this.answer.questionID) })
 			}
 		}
 	}
 </script>
+
+<style scoped lang="stylus">
+	.answer--text
+		&:first-line
+			line-height 0
+		white-space pre-line
+</style>
--- pages/ask/_id.vue
+++ pages/ask/_id.vue
@@ -1,22 +1,25 @@
 <template>
 	<v-container>
-		<v-layout row v-if="questionData.question">
-			<h2>{{questionData.question.title}}</h2>
-		</v-layout>
-		<v-layout row v-if="questionData.question">
-				asked by &nbsp; <strong>{{questionData.question.user.displayName}}</strong>
-		</v-layout>
-		<v-layout row>
-			<v-btn class="ml-0 mt-3" color="primary" @click="showAnswerForm = ! showAnswerForm" flat>Answer</v-btn>
-		</v-layout>
-		<v-flex xs4>
-			<answer-form :questionID="questionID" :user="user" v-if="showAnswerForm"/>
-		</v-flex>
-		<v-layout row mt-5>
-			<v-flex xs10 v-if="questionData.answers">
+		<v-layout>
+			<v-flex xl6>
+				<v-layout row v-if="questionData.question">
+					<h2>{{questionData.question.title}}</h2>
+				</v-layout>
+				<v-layout row v-if="questionData.question">
+					asked by &nbsp; <strong>{{questionData.question.user.displayName}}</strong>
+				</v-layout>
+				<v-divider class="mt-3"/>
+				<v-layout row>
+					<v-btn class="ml-0" color="primary" @click="showAnswerForm = ! showAnswerForm" flat><v-icon left>create</v-icon>Answer</v-btn>
+				</v-layout>
+				<answer-form :questionID="questionID" :user="user" v-if="showAnswerForm"/>
+				<v-layout row mt-5>
+					<v-flex v-if="questionData.answers">
 						<div v-for="answer in questionData.answers" :key="answer.id" class="mb-4">
 							<answer-card :answer="answer" :user="user"/>
 						</div>
+					</v-flex>
+				</v-layout>
 			</v-flex>
 		</v-layout>
 	</v-container>
@@ -44,6 +47,4 @@
 </script>
 
 <style lang="stylus">
-	.answer--text
-		margin-left 45px
 </style>
