87bba364fbb09b535ecfb20dcd99a59dad0ab03a
pyyding <kaspar.pyyding@gmail.com>
2019-04-14 22:00:28 +0300

    paddle integration

---
+++ pages/thank-you.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-card flat>
+      <v-card-text>
+        <h1 class="text-md-center">
+          Thank you 😊!
+        </h1>
+        <p class="mt-4 text-md-center">
+          You should have received a Slack invitation email. Accept the invite and follow the steps listed in
+          Slack.
+        </p>
+        <p class="mt-4 text-md-center">
+          In case of a problem, simply email me kaspar@producer.chat.
+        </p>
+        <p class="mt-4 text-md-center">
+          Yours <br>
+          Kaspar
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
